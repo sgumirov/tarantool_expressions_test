99e65e190ee11c75990de5ed12914320e7cc1c8f
Shamil Gumirov <shamil@gumirov.com>
2016-12-19 18:58:02 +0700

    shards config added

---
+++ shards/sh-test.lua
@@ -0,0 +1,43 @@
+shard = require('shard')
+box = require('box')
+
+--shard params: 
+-- CONFIG THIS!
+binport = 33131
+shards_num = 3
+
+--
+
+cfg = {
+  servers = {
+--    { uri = 'localhost:33132', zone = '2' },
+--    { uri = 'localhost:33133', zone = '3' },
+--    { uri = 'localhost:33134', zone = '4' },
+--    { uri = 'localhost:33135', zone = '5' },
+--    { uri = 'localhost:33136', zone = '6' },
+--    { uri = 'localhost:33137', zone = '7' },
+  },
+  redundancy = 1,
+  login = 'demo',
+  password = 'demo',
+  binary = binport,
+}
+for i=1,shards_num,1 do
+  cfg.servers[i]={ uri = 'localhost:'..tostring(33330+i-1), zone = tostring(i-1) }
+end
+
+
+box.cfg{
+  log_level = 5;
+  listen = cfg.binary;
+  slab_alloc_arena = 0.1;
+  wal_mode = 'none';
+}
+
+shard.check_shard = function(con)
+  return con.space.data ~= nil
+end
+shard.init(cfg)
+shard.wait_connection()
+
+print("Sharding is UP. Filling up DB...")
